be388fea7f96d40daed500ba98e11d33f627994e
samrickman <samrickman+githubpro@gmail.com>
2025-07-24 12:31:17 +0100

    feat: updated plot subtitle

--- inspect_eval_vignettes/3__run_glm_models.R
+++ inspect_eval_vignettes/3__run_glm_models.R
@@ -4,7 +4,7 @@ source("./r_utils.R")
 run_glm_models(
     "./logs/",
     get_exact_match_accuracy_dt,
-    "Accuracy metric: exact match",
+    "Accuracy metric: exact match (labels show task-specific mean)",
     "./plots/accuracy_distribution_exact_match.png",
     "./model_results_csv/exact_match_basic_comparison.csv",
     "./model_results_csv/exact_match_model_results.csv"
@@ -13,7 +13,7 @@ run_glm_models(
 run_glm_models(
     "./cosine_similarity/",
     get_cosine_accuracy_dt,
-    "Accuracy metric: cosine similarity",
+    "Accuracy metric: cosine similarity (labels show task-specific mean)",
     "./plots/accuracy_distribution_cosine_similarity.png",
     "./model_results_csv/cosine_similarity_basic_comparison.csv",
     "./model_results_csv/cosine_similarity_model_results.csv"
